f494c9636bcff7ed73e0b181fdb5a6974c71c977
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-07-23 17:00:16 -0400

    Link skills component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import General from './components/General';
 import Objective from './components/Objective';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div id="app">
       <General />
       <Objective />
+      <Skills />
     </div>
   );
 }

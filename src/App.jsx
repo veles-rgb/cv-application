c88bc567f16f788c3794b8709c9d24e2c37b8298
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-07-23 16:01:24 -0400

    Link Objective component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import General from './components/General';
+import Objective from './components/Objective';
 
 function App() {
   return (
     <div id="app">
       <General />
+      <Objective />
     </div>
   );
 }

2d652801617efc4c74b22f6735ff2f6f1d17a7e5
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-07-25 13:59:35 -0400

    Create WorkHistory component

---
+++ src/components/WorkHistory.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import '../styles/WorkHistory.css';
+
+function WorkHistory() {
+  const [editMode, setEditMode] = useState(true);
+  const [workHistoryList, setWorkHistoryList] = useState([
+    {
+      position: 'Receptionist',
+      date: '06/2021 to Current',
+      company: 'Randstad Canada',
+      location: 'Toronto, CA',
+    },
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEditMode(false);
+  };
+  function handleEdit() {
+    setEditMode(true);
+  }
+
+  const handleHistoryUpdate = (e, index, item) => {
+    const updatedHistory = [...workHistoryList];
+    updatedHistory[index][item] = e.target.value;
+    setWorkHistoryList(updatedHistory);
+  };
+
+  const handleAddHistory = () => {
+    setWorkHistoryList([
+      ...workHistoryList,
+      { position: '', date: '', company: '', location: '' },
+    ]);
+  };
+
+  const handleHistoryDelete = (e, index) => {
+    const updatedHistory = workHistoryList.filter((_, i) => i !== index);
+    setWorkHistoryList(updatedHistory);
+  };
+
+  return (
+    <div id="work-history-container">
+      {editMode ? (
+        <form onSubmit={handleSubmit}>
+          <h1>Work History</h1>
+          {workHistoryList.map((work, index) => (
+            <label key={index}>
+              <span>Position</span>
+              <input
+                type="text"
+                onChange={(e) => handleHistoryUpdate(e, index, 'position')}
+                value={work.position}
+              />
+              <span>Date</span>
+              <input
+                type="text"
+                onChange={(e) => handleHistoryUpdate(e, index, 'date')}
+                value={work.date}
+              />
+              <span>Company</span>
+              <input
+                type="text"
+                onChange={(e) => handleHistoryUpdate(e, index, 'company')}
+                value={work.company}
+              />
+              <span>Location</span>
+              <input
+                type="text"
+                onChange={(e) => handleHistoryUpdate(e, index, 'location')}
+                value={work.location}
+              />
+              <button
+                type="button"
+                onClick={(e) => handleHistoryDelete(e, index)}
+              >
+                Delete
+              </button>
+            </label>
+          ))}
+          <button type="button" onClick={handleAddHistory}>
+            Add History
+          </button>
+          <input type="submit" value="Submit" />
+        </form>
+      ) : (
+        <>
+          <h2>Work History</h2>
+          {workHistoryList.map((work) => (
+            <div className="work-history-item">
+              <div className="work-item-top">
+                <div className="work-position">{work.position},</div>
+                <div className="work-date">{work.date}</div>
+              </div>
+              <div className="work-item-bottom">
+                <div className="work-company">{work.company}</div>
+                {'-'}
+                <div className="work-location">{work.location}</div>
+              </div>
+            </div>
+          ))}
+          <button onClick={handleEdit}>Edit</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default WorkHistory;
